09edad80ecf0e8ab520bba971b07262a10780b84
Igor Araújo <igor.araujo@squadra.com.br>
2019-10-18 17:53:15 -0300

    add Booking

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -16,6 +17,8 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 // routes.post('/spots', upload.array('thumbnail'), SpotController.store); // mais de uma imagem
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 
 
